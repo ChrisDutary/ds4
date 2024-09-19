7959ae1ad05164d79bf5087237c4cf403a61840e
Christian Dutary <dutary.christian@gmail.com>
2024-09-18 20:23:55 -0500

    laboratorio#8

---
+++ Laboratorio 8/Laboratorio 8.1/Persona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_8._1
+{
+    internal class Persona
+    {
+        public string Nombre;
+
+        public int Edad;
+
+        public int NIF;
+
+        void Cumpleamos()
+        {
+            Edad++;
+        }
+
+        public Persona(string nombre, int edad, int nIF)
+        {
+            Nombre = nombre;
+            Edad = edad;
+            NIF = nIF;
+        }
+    }
+}
